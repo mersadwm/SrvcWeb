47a220caa5db91e10cfce020895db84a5a46200a
mersadwm <prince_mersad@live.com>
2019-05-29 20:43:05 +0200

    minor fix

--- js/search.js
+++ js/search.js
@@ -1,12 +1,24 @@
-< script >
-    (function() {
-        var cx = '000918350672795101152:so9bk9p7oiq';
-        var gcse = document.createElement('script');
-        gcse.type = 'text/javascript';
-        gcse.async = true;
-        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(gcse, s);
-    })(); <
-/script> <
-gcse: search > < /gcse:search>
+{
+    /* < script >
+        (function() {
+            var cx = '000918350672795101152:so9bk9p7oiq';
+            var gcse = document.createElement('script');
+            gcse.type = 'text/javascript';
+            gcse.async = true;
+            gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(gcse, s);
+        })(); <
+    /script> <
+    gcse: search > < /gcse:search> */
+}
+
+(function () {
+    var id = 'e3e8838b-dea2-4515-b37c-673476f52cbd-ws';
+    var ci_search = document.createElement('script');
+    ci_search.type = 'text/javascript';
+    ci_search.async = true;
+    ci_search.src = 'https://cse.expertrec.com/api/js/ci_common.js?id=' + id;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ci_search, s);
+})();
