8e4509e593c2debee1d626a73f3d8985a5156896
Akbar Anwarzada <akbar.anwarzada@gmail.com>
2019-05-31 11:59:39 +0200

    search migration on index

--- js/search.js
+++ js/search.js
@@ -1,19 +1,4 @@
-{
-    /* < script >
-        (function() {
-            var cx = '000918350672795101152:so9bk9p7oiq';
-            var gcse = document.createElement('script');
-            gcse.type = 'text/javascript';
-            gcse.async = true;
-            gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(gcse, s);
-        })(); <
-    /script> <
-    gcse: search > < /gcse:search> */
-}
-
-(function () {
+(function() {
     var id = 'e3e8838b-dea2-4515-b37c-673476f52cbd-ws';
     var ci_search = document.createElement('script');
     ci_search.type = 'text/javascript';
