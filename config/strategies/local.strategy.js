ef4d3af6a69e6bb5557a705fefc6147233c56c01
mersadwm <prince_mersad@live.com>
2019-06-04 13:40:25 +0200

    user reg with db connection

--- config/strategies/local.strategy.js
+++ config/strategies/local.strategy.js
@@ -1,15 +1,23 @@
 const passport = require('passport');
 const { Strategy } = require('passport-local');
-
+const sql = require('mssql');
+const debug = require('debug')('app:local.strategy');
 
 module.exports = function localStrategy() {
   passport.use(new Strategy({
     usernameField: 'username',
     passwordField: 'password',
   }, (username, password, done) => {
-    const user = {
-      username, password,
-    };
-    done(null, user);
+    (async function query() {
+      const request = new sql.Request();
+      const user = await request.input('id', sql.Int, username).query('select * from services where id=@id');
+      debug(user);
+
+      if (user.password === password) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    }());
   }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const debug = require('debug')('app:users');
+const sql = require('mssql');
+const passport = require('passport');
 
 const router = express.Router();
 
@@ -12,15 +14,32 @@ router.get('/editProfile', (req, res) => {
   res.render('editProfile');
 });
 
-router.post('/signUp', (req, res) => {
+router.route('/signUp').post((req, res) => {
   debug(req.body);
   // create user
+  const { username, password, email } = req.body;
+  (async function addUser() {
+    const request = new sql.Request();
+    const result = await request.query(`INSERT INTO users (user_name, password, email) VALUES ('${username}', '${password}', '${email}')`);
+    debug(result);
+  }());
+
   req.login(req.body, () => {
     res.redirect('/users/profile');
   });
   res.json(req.body);
 });
 
+router.route('/signin')
+  .get((req, res) => {
+    res.render('login', {
+      name: 'User',
+    });
+  })
+  .post(passport.authenticate('local', {
+    successRedirect: '/editProfile',
+    failureRedirect: '/',
+  }));
 router.route('/profile')
   .get((req, res) => {
     res.json(req.user);
