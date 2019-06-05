6694300b2d9e8aa35bb22da152399bbe9a6f130d
sep <35761081+sepehr86@users.noreply.github.com>
2019-06-06 01:00:04 +0200

    password confirm

---
+++ public/javascripts/pass_Validation.js
@@ -0,0 +1,13 @@
+let password = document.getElementById('pass'),
+ confirm_password = document.getElementById('pass_rep');
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
