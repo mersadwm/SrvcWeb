ab82af6d7b2b231f99f37838d242e4470d6fdafd
Akbar Anwarzada <akbar.anwarzada@gmail.com>
2019-06-06 11:54:12 +0200

    Address table
    Address table and procedure created + procedure to add user also created

---
+++ SQL sample commands/ALTER PROCEDURE dbo.uspAddAddress.sql
@@ -0,0 +1,25 @@
+ALTER PROCEDURE dbo.uspAddAddress
+    @pLogin NVARCHAR(50), 
+    @pAddress1 NVARCHAR(120),
+    @pAddress2 NVARCHAR(120),
+    @pAddress3 NVARCHAR(120),
+    @pPLZ INT,
+    @pCity_name nvarchar(50),
+    @pState_name nvarchar(20),
+    @pCountry nvarchar(20),
+    @responseMessage NVARCHAR(250) OUTPUT
+AS
+BEGIN
+    BEGIN TRY
+
+        INSERT INTO dbo.user_address (login_name, address_1, address_2, address_3, PLZ, city_name, state_name, Country)
+        VALUES(@pLogin, @pAddress1, @pAddress2, @pAddress3, @pPLZ, @pCity_name, @pState_name, @pCountry)
+
+       SET @responseMessage='Success'
+
+    END TRY
+    BEGIN CATCH
+        SET @responseMessage=ERROR_MESSAGE() 
+    END CATCH
+
+END
