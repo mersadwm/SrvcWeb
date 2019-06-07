6f2668b3e80ed77ee2340be40ca6d659c69d878a
sep <35761081+sepehr86@users.noreply.github.com>
2019-06-07 18:05:30 +0200

    add and update questionnair connected to database

---
+++ controllers/questionnaireController.js
@@ -0,0 +1,31 @@
+const sql = require('mssql');
+
+
+function questionnaireController() {
+  function addQuestion(question, parentKey, moreInfo, questionKey, isVisualized) {
+    const request = new sql.Request();
+    request.input('questions', question);
+    request.input('parent_key', parentKey);
+    request.input('moreinfo', moreInfo);
+    request.input('question_key', questionKey);
+    request.input('isvisualized', sql.Int, isVisualized);
+    request.query('insert into question values(@questions, @parent_key, @moreinfo, @question_key,@isvisualized)');
+  }
+
+
+  function updateQuestion(question, parentKey, moreInfo, questionKey, isVisualized) {
+    const request = new sql.Request();
+    request.input('questions', question);
+    request.input('parent_key', parentKey);
+    request.input('moreinfo', moreInfo);
+    request.input('question_key', questionKey);
+    request.input('isvisualized', sql.Int, isVisualized);
+    request.query(`update question set questions = '${question}', parent_key = '${parentKey}', moreinfo = '${moreInfo}', isvisualized = '${isVisualized}' where question_key = '${questionKey}' `);
+  }
+
+  return {
+    addQuestion,
+    updateQuestion,
+  };
+}
+module.exports = questionnaireController;
--- routes/admin.js
+++ routes/admin.js
@@ -4,19 +4,31 @@ const router = express.Router();
 
 const usersController = require('../controllers/usersController');
 
+const questionnaireController = require('../controllers/questionnaireController');
+
 const { routeProtectionAdmin } = usersController();
+
+const { addQuestion, updateQuestion } = questionnaireController();
 
 router.route('/').get((req, res) => {
   res.send('not found');
 });
 
-router.route('/addQ').all(routeProtectionAdmin).get((req, res) => {
+router.route('/addQ').get((req, res) => {
   res.render('questionnaireView/addquestion');
 })
   .post((req, res) => {
-    const {
-      questionkey, parentkey, question, isvisualized, moreinfo,
+    let {
+      questionKey, parentKey, question, isVisualized, moreInfo,
     } = req.body;
+    if (isVisualized === 'on') {
+      isVisualized = 1;
+    } else {
+      isVisualized = 0;
+    }
+    addQuestion(question, parentKey, moreInfo, questionKey, isVisualized);
+
+    res.redirect('/admin/addQ');
   });
 
 router.route('/addVis').all(routeProtectionAdmin).get((req, res) => {
@@ -27,9 +39,22 @@ router.route('/addVerb').all(routeProtectionAdmin).get((req, res) => {
   res.render('questionnaireView/addverbalanswer');
 });
 
-router.route('/updateQ').all(routeProtectionAdmin).get((req, res) => {
+router.route('/updateQ').get((req, res) => {
   res.render('questionnaireView/updateQuestion');
-});
+})
+  .post((req, res) => {
+    let {
+      questionKey, parentKey, question, isVisualized, moreInfo,
+    } = req.body;
+    if (isVisualized === 'on') {
+      isVisualized = 1;
+    } else {
+      isVisualized = 0;
+    }
+    updateQuestion(question, parentKey, moreInfo, questionKey, isVisualized);
+
+    res.redirect('/admin/updateQ');
+  });
 
 router.route('/updateA').all(routeProtectionAdmin).get((req, res) => {
   res.render('questionnaireView/updateverbalanswer');
