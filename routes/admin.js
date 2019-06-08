defc886cb10d5e1ebca61ac9e741eb147dc28ef5
mersadwm <prince_mersad@live.com>
2019-06-08 15:56:34 +0200

    code enhancement and sec. improvement in admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const debug = require('debug')('app:usersController');
-const defined = require('defined');
 
 const router = express.Router();
 
@@ -15,28 +14,12 @@ const {
 } = questionnaireController();
 
 
-const user = {
-  first_name: '',
-  last_name: '',
-  login_name: '',
-  email: '',
-  admin_rights: false,
-  profile_pic_url: '',
-  address_1: '',
-  address_2: '',
-  address_3: '',
-  city_name: '',
-  state_name: '',
-  Country: '',
-  PLZ: '',
-};
-
 router.route('/').get((req, res) => {
   res.send('not found');
 });
 
 router.route('/addQ').all(routeProtectionAdmin).get((req, res) => {
-  res.render('questionnaireView/addquestion', { user: defined(req.user, user), logged: req.isAuthenticated() });
+  res.render('questionnaireView/addquestion', { user: req.user, logged: req.isAuthenticated() });
 })
   .post((req, res) => {
     let {
@@ -50,7 +33,7 @@ router.route('/addQ').all(routeProtectionAdmin).get((req, res) => {
   });
 
 router.route('/addVis').all(routeProtectionAdmin).get((req, res) => {
-  res.render('questionnaireView/addvisualanswers', { user: defined(req.user, user), logged: req.isAuthenticated() });
+  res.render('questionnaireView/addvisualanswers', { user: req.user, logged: req.isAuthenticated() });
 })
   .post((req, res) => {
     const {
@@ -62,7 +45,7 @@ router.route('/addVis').all(routeProtectionAdmin).get((req, res) => {
   });
 
 router.route('/addVerb').all(routeProtectionAdmin).get((req, res) => {
-  res.render('questionnaireView/addverbalanswer', { user: defined(req.user, user), logged: req.isAuthenticated() });
+  res.render('questionnaireView/addverbalanswer', { user: req.user, logged: req.isAuthenticated() });
 })
   .post((req, res) => {
     const {
@@ -74,7 +57,7 @@ router.route('/addVerb').all(routeProtectionAdmin).get((req, res) => {
   });
 
 router.route('/updateQ').all(routeProtectionAdmin).get((req, res) => {
-  res.render('questionnaireView/updateQuestion', { user: defined(req.user, user), logged: req.isAuthenticated() });
+  res.render('questionnaireView/updateQuestion', { user: req.user, logged: req.isAuthenticated() });
 })
   .post((req, res) => {
     let {
@@ -88,7 +71,7 @@ router.route('/updateQ').all(routeProtectionAdmin).get((req, res) => {
   });
 
 router.route('/updateA').all(routeProtectionAdmin).get((req, res) => {
-  res.render('questionnaireView/updateverbalanswer', { user: defined(req.user, user), logged: req.isAuthenticated() });
+  res.render('questionnaireView/updateverbalanswer', { user: req.user, logged: req.isAuthenticated() });
 })
   .post((req, res) => {
     const {
